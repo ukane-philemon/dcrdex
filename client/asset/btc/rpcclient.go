e7b69720614d1d4f0b6048c8aa7302866d952aa3
Philemon Ukane <ukanephilemon@gmail.com>
2024-01-19 00:39:30 +0100

    export useful methods
    Signed-off-by: Philemon Ukane <ukanephilemon@gmail.com>

--- client/asset/btc/rpcclient.go
+++ client/asset/btc/rpcclient.go
@@ -1093,7 +1093,7 @@ func FindRedemptionsInMempool(
 			logAbandon(fmt.Sprintf("getrawtransaction error for tx hash %v: %v", txHash, err))
 			return
 		}
-		newlyDiscovered := findRedemptionsInTxWithHasher(ctx, segwit, reqs, tx, chainParams, hashTx)
+		newlyDiscovered := FindRedemptionsInTxWithHasher(ctx, segwit, reqs, tx, chainParams, hashTx)
 		for outPt, res := range newlyDiscovered {
 			discovered[outPt] = res
 		}
@@ -1125,7 +1125,7 @@ func SearchBlockForRedemptions(
 	discovered = make(map[OutPoint]*FindRedemptionResult, len(reqs))
 
 	for _, msgTx := range msgBlock.Transactions {
-		newlyDiscovered := findRedemptionsInTxWithHasher(ctx, segwit, reqs, msgTx, chainParams, hashTx)
+		newlyDiscovered := FindRedemptionsInTxWithHasher(ctx, segwit, reqs, msgTx, chainParams, hashTx)
 		for outPt, res := range newlyDiscovered {
 			discovered[outPt] = res
 		}
